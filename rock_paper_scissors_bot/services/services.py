283df3d8163e3c6367372cc256d9a0c0fab491ef
Mikhail Kryzhanovskiy <kmsrus@gmail.com>
2023-02-14 22:33:43 +0300

    added comments

--- rock_paper_scissors_bot/services/services.py
+++ rock_paper_scissors_bot/services/services.py
@@ -3,10 +3,13 @@ import random
 from lexicon.lexicon_ru import LEXICON_RU
 
 
+# Функция, возвращающая случайный выбор бота в игре
 def get_bot_choice() -> str:
     return random.choice(['rock', 'paper', 'scissors'])
 
 
+# Функция, возвращающая ключ из словаря, по которому
+# хранится значение, передаваемое как аргумент - выбор пользователя
 def _normalize_user_answer(user_answer: str) -> str:
     for key in LEXICON_RU:
         if LEXICON_RU[key] == user_answer:
@@ -14,6 +17,7 @@ def _normalize_user_answer(user_answer: str) -> str:
     raise Exception
 
 
+# Функция, определяющая победителя
 def get_winner(user_choice: str, bot_choice: str) -> str:
     user_choice = _normalize_user_answer(user_choice)
     rules: dict[str, str] = {'rock': 'scissors',
