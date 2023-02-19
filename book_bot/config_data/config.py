1daace82e808aee2d55aece229d4d52bfd93807f
Mikhail Kryzhanovskiy <kmsrus@gmail.com>
2023-02-19 14:48:38 +0300

    added book_bot structure

---
+++ book_bot/config_data/config.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    token: str            # Токен для доступа к телеграм-боту
+    admin_ids: list[int]  # Список id администраторов бота
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+# Создаем функцию, которая будет читать файл .env и возвращать
+# экземпляр класса Config с заполненными полями token и admin_ids
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(
+                    token=env('BOT_TOKEN'),
+                    admin_ids=list(map(int, env.list('ADMIN_IDS')))))
