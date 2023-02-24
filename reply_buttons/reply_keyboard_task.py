100d44cb7c2557f6084c8bc5fac8b336b6d4b804
Mikhail Kryzhanovskiy <kmsrus@gmail.com>
2023-02-25 00:10:04 +0300

    added reply keyboard example for task

---
+++ reply_buttons/reply_keyboard_task.py
@@ -0,0 +1,35 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import (KeyboardButton, Message,
+                           ReplyKeyboardMarkup)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+API_TOKEN: str = 'BOT TOKEN HERE'
+
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+# Создаем список списков с кнопками
+keyboard: list[KeyboardButton] = [
+    KeyboardButton(text=str(i)) for i in range(1, 8)]
+
+# Инициализируем билдер
+builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+
+builder.row(*keyboard, width=3)
+
+# Создаем объект клавиатуры, добавляя в него кнопки
+my_keyboard: ReplyKeyboardMarkup = builder.as_markup(resize_keyboard=True)
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text='Экспериментируем с обычными кнопками',
+                         reply_markup=my_keyboard)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
