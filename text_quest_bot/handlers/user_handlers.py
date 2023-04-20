c3e998f79dc78ffffd67c90460f17c5de0e01f6e
Mikhail Kryzhanovskiy <kmsrus@gmail.com>
2023-04-20 17:00:08 +0300

    commited some work on text_quest_bot

--- text_quest_bot/handlers/user_handlers.py
+++ text_quest_bot/handlers/user_handlers.py
@@ -4,10 +4,14 @@ from aiogram import Router
 from aiogram.filters import Command, CommandStart, StateFilter, Text
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, ReplyKeyboardRemove
 from keyboards.kb_utils import create_inline_kb, create_reply_kb
 from lexicon.lexicon import LEXICON
-from states.states import FSMStartQuest
+from states.states import FSMStartQuest, FSMEngineeringCompartment
+
+T_2: int = 0
+T_3: int = 0
+T_4: int = 0
 
 user_router: Router = Router()
 
@@ -40,11 +44,12 @@ async def process_cancel_command_state(message: Message, state: FSMContext):
 # и начинать квест, переводя игрока в состояние
 @user_router.message(Command(commands='beginning'))
 async def process_beginning_command(message: Message, state: FSMContext):
+    await state.update_data(backpack=set())
     await message.answer(LEXICON['/beginning'])
-    await asyncio.sleep(3)
+    await asyncio.sleep(T_2)
     for i in range(1, 6):
         await message.answer(LEXICON[f'beginning_{i}'])
-        await asyncio.sleep(4)
+        await asyncio.sleep(T_4)
     reply_kb = create_reply_kb(1, 'yes_ready', 'maybe_ready', 'no_ready')
     await message.answer(text=LEXICON['beginning_6'],
                          reply_markup=reply_kb)
@@ -57,26 +62,104 @@ async def process_beginning_command(message: Message, state: FSMContext):
                      StateFilter(FSMStartQuest.ready_state))
 async def process_ready_answer(message: Message, state: FSMContext):
     if message.text == LEXICON['yes_ready']:
-        await message.answer(LEXICON['yes_ready_answer'])
+        await message.answer(LEXICON['yes_ready_answer'],
+                             reply_markup=ReplyKeyboardRemove())
     else:
-        await message.answer(LEXICON['maybe_ready_answer'])
-    await asyncio.sleep(2)
+        await message.answer(LEXICON['maybe_ready_answer'],
+                             reply_markup=ReplyKeyboardRemove())
+    await asyncio.sleep(T_2)
     for i in range(1, 5):
         await message.answer(LEXICON[f'quest_started_{i}'])
-        await asyncio.sleep(4)
-    inline_kb = create_inline_kb(1, 'look_around', 'see_backpack', 'get_out')
+        await asyncio.sleep(T_4)
+    reply_kb = create_reply_kb(
+                        1, 'look_around',
+                        'see_backpack',
+                        'get_out_engine_compartment')
     await message.answer(text=LEXICON['start_state_available_actions'],
-                         reply_markup=inline_kb)
+                         reply_markup=reply_kb)
     await state.set_state(FSMStartQuest.start_state)
+
+
+# Этот хэндлер будет срабатывать на отправку сообщения "Осмотреться"
+# в состоянии FSMStartQuest.start_state
+@user_router.message(Text(text=LEXICON['look_around']),
+                     StateFilter(FSMStartQuest.start_state))
+async def process_look_around_eng_msg(message: Message):
+    for i in range(1, 5):
+        await message.answer(LEXICON[f'look_around_eng_answ_{i}'])
+        await asyncio.sleep(T_4)
+    reply_kb = create_reply_kb(1, 'see_backpack',
+                               'get_out_engine_compartment')
+    await message.answer(text=LEXICON['look_around_eng_answ_5'],
+                         reply_markup=reply_kb)
+
+
+# Этот хэндлер будет срабатывать на нажатие кнопки "Проверить рюкзак"
+# в любом состоянии, кроме дефолтного
+@user_router.message(Text(text=LEXICON['see_backpack']),
+                     ~StateFilter(default_state))
+async def process_see_backpack_press(message: Message,
+                                     state: FSMContext):
+    backpack = await state.get_data()
+    if backpack['backpack']:
+        backpack_items_kb = create_inline_kb(1, *backpack['backpack'])
+        await message.answer(text=LEXICON['backpack_items'],
+                             reply_markup=backpack_items_kb)
+    else:
+        reply_kb = create_reply_kb(1, 'look_around',
+                                   'see_backpack',
+                                   'get_out_engine_compartment')
+        await message.answer(text=LEXICON['backpack_empty'],
+                             reply_markup=reply_kb)
+
+
+# Этот хэндлер будет срабатывать на отправку сообщения
+# "Покинуть двигательный отсек" и переводить в состояние
+# FSMEngineeringCompartment.first_state
+@user_router.message(Text(text=LEXICON['get_out_engine_compartment']),
+                     StateFilter(FSMStartQuest.start_state))
+async def process_get_out_engine_comprtment_msg(message: Message,
+                                                state: FSMContext):
+    await state.set_state(FSMEngineeringCompartment.first_state)
+    for i in range(1, 3):
+        await message.answer(
+                text=LEXICON[f'engineering_compartment_{i}'])
+        await asyncio.sleep(T_3)
+    reply_kb = create_reply_kb(1, 'look_around',
+                               'see_backpack',
+                               'return_to_engine_compartment')
+    await message.answer(text=LEXICON['engineering_compartment_3'],
+                         reply_markup=reply_kb)
+
+
+# Этот хэндлер будет срабатывать на сообщение "Осмотреться"
+# в состоянии FSMStartQuest.start_state
+@user_router.message(Text(text=LEXICON['look_around']),
+                     StateFilter(FSMEngineeringCompartment.first_state))
+async def process_look_around_engineerig_msg(message: Message):
+    for i in range(1, 5):
+        await message.answer(
+                text=LEXICON[f'look_around_engineering_answ_{i}'])
+        await asyncio.sleep(T_4)
+    reply_kb = create_reply_kb(2, 'see_backpack',
+                               'return_to_engine_compartment',
+                               'go_to_med_compartment',
+                               'go_to_bridge',
+                               'take_spare_parts',
+                               'take_tools')
+    await message.answer(
+                    text=LEXICON['look_around_engineering_answ_5'],
+                    reply_markup=reply_kb)
 
 
 # Этот хэндлер будет срабатывать на отказ игрока выполнить миссию
 @user_router.message(Text(text=LEXICON['no_ready']),
                      StateFilter(FSMStartQuest.ready_state))
 async def process_no_ready_answer(message: Message, state: FSMContext):
-    await message.answer(LEXICON['no_ready_answer'])
-    await asyncio.sleep(3)
+    await message.answer(LEXICON['no_ready_answer'],
+                         reply_markup=ReplyKeyboardRemove())
+    await asyncio.sleep(T_3)
     await message.answer(LEXICON['no_ready_answer_1'])
-    await asyncio.sleep(3)
+    await asyncio.sleep(T_3)
     await message.answer(LEXICON['quest_failed'])
     await state.clear()
--- text_quest_bot/states/states.py
+++ text_quest_bot/states/states.py
@@ -4,3 +4,7 @@ from aiogram.filters.state import State, StatesGroup
 class FSMStartQuest(StatesGroup):
     ready_state = State()
     start_state = State()
+
+
+class FSMEngineeringCompartment(StatesGroup):
+    first_state = State()
