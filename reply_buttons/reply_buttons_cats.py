1e028c7ba313371c0e6acd2420c44f241387799b
kmsint <kmsrus@gmail.com>
2023-08-16 00:24:30 +0300

    added reply_buttons_cats.py without Text filter

---
+++ reply_buttons/reply_buttons_cats.py
@@ -0,0 +1,47 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+API_TOKEN: str = 'BOT TOKEN HERE'
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+# Создаем объекты кнопок
+button_1: KeyboardButton = KeyboardButton(text='Собак 🦮')
+button_2: KeyboardButton = KeyboardButton(text='Огурцов 🥒')
+
+# Создаем объект клавиатуры, добавляя в него кнопки
+keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+                                    keyboard=[[button_1, button_2]],
+                                    resize_keyboard=True,
+                                    one_time_keyboard=True)
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+# и отправлять в чат клавиатуру
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text='Чего кошки боятся больше?',
+                         reply_markup=keyboard)
+
+
+# Этот хэндлер будет срабатывать на ответ "Собак 🦮" и удалять клавиатуру
+@dp.message(F.text == 'Собак 🦮')
+async def process_dog_answer(message: Message):
+    await message.answer(text='Да, несомненно, кошки боятся собак. '
+                              'Но вы видели как они боятся огурцов?')
+
+
+# Этот хэндлер будет срабатывать на ответ "Огурцов 🥒" и удалять клавиатуру
+@dp.message(F.text == 'Огурцов 🥒')
+async def process_cucumber_answer(message: Message):
+    await message.answer(text='Да, иногда кажется, что огурцов '
+                              'кошки боятся больше')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
