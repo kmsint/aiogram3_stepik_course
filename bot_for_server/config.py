d308fac1a787fb532f88255a0560b1e41e1ad362
kmsint <kmsrus@gmail.com>
2023-08-28 16:26:22 +0300

    added bot for deploy example

---
+++ bot_for_server/config.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    token: str            # Токен для доступа к телеграм-боту
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+# Создаем функцию, которая будет читать файл .env и возвращать экземпляр
+# класса Config с заполненными полями token и admin_ids
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
