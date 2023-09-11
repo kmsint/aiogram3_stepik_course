1e6b09263c40b52784024ce7f9bb0651f8909965
kmsint <kmsrus@gmail.com>
2023-09-11 21:35:48 +0300

    added polling and long polling exapmles

---
+++ polling/long_polling_example.py
@@ -0,0 +1,26 @@
+import requests
+import time
+
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '5424991242:AAGwomxQz1p46bRi_2m3V7kvJlt5RjK9xr0'
+offset = -2
+timeout = 60
+updates: dict
+
+
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True:
+    start_time = time.time()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            do_something()
+
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
---
+++ polling/polling_example.py
@@ -0,0 +1,27 @@
+import requests
+import time
+
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = 'BOT TOKEN HERE'
+
+offset = -2
+updates: dict
+
+
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True:
+    start_time = time.time()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            do_something()
+
+    time.sleep(3)
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
